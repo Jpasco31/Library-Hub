cc34333616445856ffb4fdc8744232c0a0f821c3
Jpasco31 <112312834+Jpasco31@users.noreply.github.com>
2023-05-27 23:17:25 +0800

    Update borrow.php

--- library-hub/borrow.php
+++ library-hub/borrow.php
@@ -4,10 +4,11 @@ session_start();
     include("connection.php");
     include("functions.php");
 
-    $user_data = check_login($con);
-    
     $bookId = NULL;
     $bookTitle = NULL;
+    
+    $user_data = check_login($con);
+
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['searchButton'])) {
         $bookId = $_POST['bookId'];
@@ -20,25 +21,33 @@ session_start();
             $bookTitle = $row['bookTitle'];
         } else {
             // Book not found
+            $bookId = "Book not found";
             $bookTitle = "Book not found";
         }
     }
 
+    date_default_timezone_set('Asia/Manila');
+    $currentDate = date('Y-m-d');
+
+    // Borrow book
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['borrowButton'])) {
         $currentUserId = $user_data['accountId'];
         $bookIdSelect = $_POST['bookIdSelect'];
         $borrowDate = $_POST['borrowDate'];
-
-        $query = "INSERT INTO borrow (bookId, accountId, borrowDate) VALUES('$bookIdSelect', '$currentUserId', '$borrowDate')";
-        
+        $toReturnDate = date('Y-m-d', strtotime($borrowDate . ' + 20 days'));
+      
+        // Insert the borrow record into the database
+        $query = "INSERT INTO borrow (bookId, accountId, borrowDate, toReturnDate,late, returned) VALUES ('$bookIdSelect', '$currentUserId', '$borrowDate', '$toReturnDate','0','0')";
+      
         mysqli_query($con, $query);
     }
 
+    // Return book
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['returnButton'])) {
         $borrowId = $_POST['borrowId'];
         $returnDate = $_POST['returnDate'];
     
-        $query = "UPDATE borrow SET returnDate = '$returnDate' WHERE borrowId = '$borrowId'";
+        $query = "UPDATE borrow SET returnDate = '$returnDate', returned = 1 WHERE borrowId = '$borrowId'";
     
         mysqli_query($con, $query);
     }
@@ -71,88 +80,127 @@ session_start();
     <section class="container-fluid mt-5">
         <div class="row px-5 pb-3">
             <!-- Borrow -->
-            <div class="col-md-4">
-                <div class="container custom-container mt-4 mb-5 border p-4">
-                    <h3 class="fw-bold">Borrow</h3>
-                    <form class="container p-3 bg-white rounded-3 pt-4 mt-1" method="post">
-                        <div class="form-group mb-2">
-                            <label for="bookId">Book ID</label>
-                            <input type="text" class="form-control custom-input" id="bookId" placeholder="Enter Book ID" name="bookId">
-                        </div>
-                        <div class="d-grid gap-2">
+            <?php
+            if (!$user_data['admin']) {
+                echo '
+                <div class="col-md-4">
+                    <div class="container mt-4 mb-5 border p-4">
+                        <h3 class="fw-bold">Borrow</h3>
+                        <p><em>Duration of borrowed books - 20 days from borrow date return</em></p>
+                        <p><em>Please return book on or before return date</em></p>
+                        <form class="container p-3 bg-white rounded-3 pt-4 mt-1" method="post" onsubmit="return validateBorrowDate()">
+                            <div class="form-group mb-2">
+                                <label for="bookId">Book ID</label>
+                                <input type="text" class="form-control custom-input" id="bookId" placeholder="Enter Book ID" name="bookId">
+                            </div>
+                            <div class="d-grid gap-2">
                                 <input class="btn btn-primary p-3 my-3 rounded-5" type="submit" value="Search" name="searchButton">
-                        </div>
-                        <div class="form-group mb-2">
-                            <label for="bookId">Book ID</label>
-                            <input type="text" class="form-control custom-input" id="bookId" name="bookIdSelect" value="<?php echo $bookId; ?>">
-                        </div>
-                        <div class="form-group mb-2">
-                            <label for="bookTitle">Book Title</label>
-                            <input type="text" class="form-control custom-input" id="bookTitle" value="<?php echo $bookTitle; ?>">
-                        </div>
-                        <div class="form-group mb-2">
-                            <label for="borrowDate">Borrow Date</label>
-                            <input type="date" class="form-control custom-input" id="borrowDate" name="borrowDate">
-                        </div>
-                        <div class="d-grid gap-2">
-                            <input class="btn btn-primary p-3 my-3 rounded-5" type="submit" value="Borrow" name="borrowButton">
-                        </div>
-                    </form>
-                </div>
-            </div>
+                            </div>
+                            <div class="form-group mb-2">
+                                <label for="bookIdSelect">Book ID</label>
+                                <input type="text" class="form-control custom-input" id="bookIdSelect" name="bookIdSelect" value="' . $bookId . '" readonly>
+                            </div>
+                            <div class="form-group mb-2">
+                                <label for="bookTitle">Book Title</label>
+                                <input type="text" class="form-control custom-input" id="bookTitle" value="' . $bookTitle . '" readonly>
+                            </div>
+                            <div id="borrowDateGroup" class="form-group mb-2">
+                                <label for="borrowDate">Borrow Date</label>
+                                <input type="date" class="form-control custom-input" id="borrowDate" name="borrowDate" value="' . $currentDate . '" required>
+                            </div>
+                            <div class="d-grid gap-2">
+                                <input class="btn btn-primary p-3 my-3 rounded-5" type="submit" value="Borrow" name="borrowButton">
+                            </div>
+                        </form>
+                    </div>
+                </div>';
+            }
+            ?>
 
-      <!-- To Return -->
+
+            <!-- To Return -->
             <div class="col-md-4">
-                <div class="container custom-container mt-4 mb-5 border p-4">
+                <div class="container mt-4 mb-5 border p-4">
                     <h3 class="fw-bold">To Return</h3>
+                    <p><em>Red Highlight- Return Date has passed<br> RETURN BOOK ASAP</em></p>
                     <div class="table-responsive-xxl">
                         <?php
                         // Create a query to retrieve data from the borrow table
-                        $sql = "SELECT borrow.borrowId, borrow.bookId, borrow.borrowDate, borrow.returnDate, borrow.accountId, books.bookTitle
-                        FROM borrow
-                        INNER JOIN books ON borrow.bookId = books.bookId
-                        WHERE borrow.accountId = '{$user_data['accountId']}'";
+                        if ($user_data['accountId'] == 1) {
+                            $sql = "SELECT borrow.borrowId, borrow.bookId, borrow.borrowDate, borrow.toReturnDate, borrow.returnDate, borrow.late, borrow.accountId, books.bookTitle
+                                    FROM borrow
+                                    INNER JOIN books ON borrow.bookId = books.bookId
+                                    WHERE borrow.returned = 0";
+                        } else {
+                            $sql = "SELECT borrow.borrowId, borrow.bookId, borrow.borrowDate, borrow.toReturnDate, borrow.returnDate, borrow.late, borrow.accountId, books.bookTitle
+                                    FROM borrow
+                                    INNER JOIN books ON borrow.bookId = books.bookId
+                                    WHERE borrow.accountId = '{$user_data['accountId']}' AND borrow.returned = 0";
+                        }
 
                         // Execute the query
                         $result = mysqli_query($con, $sql);
-                        // Check if there are any rows returned
+
+                        $currentDate = date('Y-m-d');
+                        while ($returnCheck = mysqli_fetch_assoc($result)) {
+                            if ($currentDate > $returnCheck['toReturnDate']) {
+                                $lateUpdateQuery = "UPDATE borrow SET late = 1 WHERE borrowId = '{$returnCheck['borrowId']}'";
+                                mysqli_query($con, $lateUpdateQuery);
+                            }
+                        }
+
+                        // Execute the query again to fetch the updated result set
+                        $result = mysqli_query($con, $sql);
+
+                        // Check if there are any rows returned (excluding the header row)
                         if (mysqli_num_rows($result) > 0) {
                             echo "<table class='table table-hover'>
-                                    <thead>
-                                        <tr>
-                                            <th>Borrow ID</th>
-                                            <th>Book ID</th>
-                                            <th>Book Title</th>
-                                            <th>Borrow Date</th>
+                                <thead>
+                                    <tr>
+                                        <th>Borrow ID</th>";
+                                        if ($user_data['accountId'] == 1) {
+                                            echo "<th>Account ID</th>";
+                                        }
+                                        echo "<th>Book ID</th>
+                                        <th>Book Title</th>
+                                        <th>Borrow Date</th>
+                                        <th>To Return Date</th>
                                         </tr>
-                                    </thead>
-                                    <tbody>";
+                                        </thead>
+                                        <tbody>";
 
                             // Loop through each row and display the data
                             while ($row = mysqli_fetch_assoc($result)) {
-                                if($row['returnDate'] == NULL){
-                                    echo "<tr>";
+                                    echo "<tr";
+                                    if ($row['late'] == 1) {
+                                        echo " style='background-color: red;'";
+                                    }
+                                    echo ">";
                                     echo "<td>{$row['borrowId']}</td>";
+                                    if ($user_data['accountId'] == 1) {
+                                        echo "<td>{$row['accountId']}</td>";
+                                    }
                                     echo "<td>{$row['bookId']}</td>";
                                     echo "<td>{$row['bookTitle']}</td>";
                                     echo "<td>{$row['borrowDate']}</td>";
+                                    echo "<td>{$row['toReturnDate']}</td>";
                                     echo "</tr>";
-                                }
+        
                             }
 
                             echo "</tbody></table>";
                         } else {
-                            echo "<p>No borrow data available.</p>";
+                            echo "<p>No books borrowed.</p>";
                         }
                         ?>
                     </div>
                 </div>
             </div>
             
-            <!-- Retun -->
+            <!-- Return -->
             <div class="col-md-4">
-                <div class="container custom-container mt-4 mb-5 border p-4">
-                    <h3 class="fw-bold">Borrow</h3>
+                <div class="container mt-4 mb-5 border p-4">
+                    <h3 class="fw-bold">Return</h3>
                     <form class="container p-3 bg-white rounded-3 pt-4 mt-1" method="post">
                         <div class="form-group mb-2">
                             <label for="borrowId">Borrow ID</label>
@@ -160,7 +208,7 @@ session_start();
                         </div>
                         <div class="form-group mb-2">
                             <label for="returnDate">Return Date</label>
-                            <input type="date" class="form-control custom-input" id="returnDate" name="returnDate">
+                            <input type="date" class="form-control custom-input" id="returnDate" name="returnDate" value="<?php echo $currentDate; ?>" required>
                         </div>
                         <div class="d-grid gap-2">
                                 <input class="btn btn-primary p-3 my-3 rounded-5" type="submit" value="Return" name="returnButton">
@@ -178,6 +226,7 @@ session_start();
     <?php include 'footer.php';?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
 
 </body>
 </html>
