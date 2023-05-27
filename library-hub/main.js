e779a7d869a6e4c270fb4ce929b960019229bf6c
Jpasco31 <112312834+Jpasco31@users.noreply.github.com>
2023-05-27 23:20:11 +0800

    Update main.js

--- library-hub/main.js
+++ library-hub/main.js
@@ -9,3 +9,30 @@ function scrollToBookPage() {
 function myFunction() {
     alert("Not admin");
   }
+
+  function toggleSynopsis(bookId) {
+    var synopsisElement = document.getElementById("synopsis-" + bookId);
+    if (synopsisElement.style.display === "none") {
+        synopsisElement.style.display = "block";
+    } else {
+        synopsisElement.style.display = "none";
+    }
+}
+
+function openSynopsisModal(bookId) {
+    var modalElement = document.getElementById("modal-" + bookId);
+    var modal = new bootstrap.Modal(modalElement);
+    modal.show();
+}
+
+function validateBorrowDate() {
+    var borrowButton = document.getElementsByName("borrowButton")[0];
+    var borrowDate = document.getElementById("borrowDate");
+
+    if (borrowButton && borrowButton.clicked && borrowDate && !borrowDate.value) {
+        borrowDate.setCustomValidity("Borrow date is required.");
+        return false;
+    }
+
+    return true;
+}
